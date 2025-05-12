a7125374eae8cc9060fd4ea6fb66efc461973f10
MennovdLinde <mennovandelinde@gmail.com>
2025-05-12 23:21:12 +0200

    new

--- server-new.js
+++ server-new.js
@@ -9,7 +9,12 @@ const { createCanvas } = require("canvas");
 const app = express();
 const upload = multer({ dest: "uploads/" });
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://mennovdlinde.github.io',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
